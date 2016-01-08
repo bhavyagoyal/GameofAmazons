2e48d2f38e138c11ed67b6217213fb125eca0aa8
Bhavya Goyal <bhavyagoyal777@gmail.com>
2016-01-08 22:48:41 +0530

    First

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define MOD 1000000009 
+#define MAX(a,b) ( (a) > (b) ? (a) : (b))
+#define MIN(a,b) ( (a) < (b) ? (a) : (b))
+
+int main(){
+	int DP[10][10];
+	int player;
+	FOR(i,10){
+		FOR(j,10){
+			scanf("%d",&DP[i][j]);
+		}
+	}
+	scanf("%d",&player);
+
+
+	return 0;
+}
