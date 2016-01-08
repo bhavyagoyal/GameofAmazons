508ffe8f646b6e5ec256f7cd36c2f32cb795b6ab
Bhavya Goyal <bhavyagoyal777@gmail.com>
2016-01-08 23:43:19 +0530

    Adding Evaluator function

--- main.cpp
+++ main.cpp
@@ -7,8 +7,55 @@ using namespace std;
 #define MAX(a,b) ( (a) > (b) ? (a) : (b))
 #define MIN(a,b) ( (a) < (b) ? (a) : (b))
 
+int DP[10][10];
+
+
+int mobility(){
+	int count=0;
+	int sign=1;
+	FOR(i,10){
+		FOR(j,10){
+			if(DP[i][j] == 1 ||  DP[i][j]==2){
+				if(DP[i][j] == 2){
+					sign = -1;
+				}
+				else{
+					sign=1;
+				}
+				std::vector<int> a;
+				a.push_back(i);
+				if(i<9){
+					a.push_back(i+1);
+				}
+				if(i>0){
+					a.push_back(i-1);
+				}
+				std::vector<int> b;
+				b.push_back(j);
+				if(j<9){
+					b.push_back(j+1);
+				}
+				if(j>0){
+					b.push_back(j-1);
+				}
+				FOR(k,a.size()){
+					FOR(l,b.size()){
+						if(DP[a[k]][b[l]] == 0){
+							count = count+sign;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int evaluator(){
+	int ans = 0;
+	ans = ans+mobility();
+}
+
 int main(){
-	int DP[10][10];
 	int player;
 	FOR(i,10){
 		FOR(j,10){
@@ -16,6 +63,7 @@ int main(){
 		}
 	}
 	scanf("%d",&player);
+
 
 
 	return 0;
